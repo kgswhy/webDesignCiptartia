f1fb4793cc5d92206e8847db4f2964d7bb789673
Kgs. Wahyu <kgswahyu80@gmail.com>
2023-04-06 20:05:51 +0700

    setup server + adding models + controllers + route + fixing directory

---
+++ server/src/models/userModel.js
@@ -0,0 +1,9 @@
+// User
+// id (string)
+// username (string)
+// email (string)
+// password (string)
+// role (string)
+// avatar (string) // untuk menyimpan foto profil user => by buffer biasanya
+// created_at (date)
+// updated_at (date)
