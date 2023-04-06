2142caf0d36a3802ad1664455afca1f4fd7cd338
Kgs. Wahyu <kgswahyu80@gmail.com>
2023-04-06 22:54:52 +0700

    setup database connection + adding userModel Crud

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORTdwaw || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
