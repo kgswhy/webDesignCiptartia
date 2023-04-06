53a4f8622425ed022377a9170573094bde6a9c1b
Kgs. Wahyu <kgswahyu80@gmail.com>
2023-04-06 23:12:04 +0700

    adding transactionModel CRUD

--- server/src/models/transactionModel.js
+++ server/src/models/transactionModel.js
@@ -1,9 +1,55 @@
-// Transaction
-// id (string)
-// order_id (string)
-// status (string) // untuk membedakan status transaksi (belum diproses, sedang diproses, sudah selesai)
-// payment_date (date)
-// payment_amount (number)
-// payment_proof (string) // untuk menyimpan bukti pembayaran
-// created_at (date)
-// updated_at (date)
+const db = require('../config/dbConnect');
+
+const Transaction = {
+    create(newTransaction, callback) {
+        db.query('INSERT INTO transactions SET ?', newTransaction, (err, result) => {
+            if (err) throw err;
+            callback(result);
+            console.log('Transaction created', { id: result.insertId, ...newTransaction });
+            return { id: result.insertId, ...newTransaction };
+        });
+    },
+    findAll(callback) {
+        db.query('SELECT * FROM transactions', (err, result) => {
+            if (err) throw err;
+            callback(result);
+            console.log('Found transactions', result);
+            return result;
+        });
+    },
+    findById(id, callback) {
+        db.query('SELECT * FROM transactions WHERE id = ?', [id], (err, result) => {
+            if (err) throw err;
+            callback(result);
+            if (result.length) {
+                console.log('Found transaction', result[0]);
+                return result[0];
+            }
+            return result({ message: 'Transaction not found' }, null);
+        });
+    },
+    update(id, transaction, callback) {
+        db.query('UPDATE transactions SET ? WHERE id = ?', [transaction, id], (err, result) => {
+            if (err) throw err;
+            callback(result);
+            if (result.affectedRows === 0) {
+                return result({ message: 'Transaction not found' }, null);
+            }
+            console.log('Updated transaction', { id, ...transaction });
+            return { id, ...transaction };
+        });
+    },
+    delete(id, callback) {
+        db.query('DELETE FROM transactions WHERE id = ?', [id], (err, result) => {
+            if (err) throw err;
+            callback(result);
+            if (result.affectedRows === 0) {
+                return result({ message: 'Transaction not found' }, null);
+            }
+            console.log('Deleted transaction', id);
+            return result;
+        });
+    },
+};
+
+module.exports = Transaction;
